f1d7ea95dd64ae5b4b93c04609e89319c14255bd
fingersome <tedsouth@outlook.com>
2017-03-24 03:50:38 +0000

    Many things

--- contactform.php
+++ contactform.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "info@percycentre.org";
+    $email_to = "teddy@percycentre.org";
     $email_subject = "Your email subject line";
 
     function died($error) {
